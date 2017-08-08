ad152f09d299381cfbdaae24b24f032b127c63a1
YooHannah <1942063705@qq.com>
2017-08-08 18:28:39 +0800

    about this

--- test/test.js
+++ test/test.js
@@ -4,46 +4,52 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj) //setTimeout 第一个参数函数里面的this
           done()
         }, 0)
       }
     }
     obj.say()
-  }) 
+  })
 
-  it('global', function () {
+ it('global', function () {
+    var obj=null;
     function test() {
       // this 是什么？想想为什么？
       this.should.equal(null)
     }
-    test()
+    test.bind(obj);
   })
 
   describe('bind', function () {
     it('bind undefined', function () {
       var obj = {
         say: function () {
+          var that=this;
           function _say() {
-            // this 是什么？想想为什么？
-            this.should.equal(null)
+            // this 是什么？想想为什么？this指向window·
+            that.should.equal(null)
           }
           return _say.bind(obj)
         }()
       }
-      obj.say()
+      var temp = null;
+      obj.say.bind(temp);
     })
 
     it('bind normal', function () {
       var obj = {}
       obj.say = function () {
         function _say() {
-          // this 是什么？想想为什么？
+          // this 是什么？想想为什么？this指向调用say函数的对象
           this.should.equal(null)
         }
         return _say.bind(obj)
       }()
-      obj.say()
+      var temp = null;
+      obj.say.bind(temp);
+      // obj.say();
     })
+
   })
 })
